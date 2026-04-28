ddba5e46169b69fd1ea392b519e03e7d81d3f188
Abdelrahman Ashraf <8effort@gmail.com>
2026-04-29 01:02:07 +0300

    new create app

--- android/android/app/build.gradle.kts
+++ android/android/app/build.gradle.kts
@@ -6,15 +6,13 @@ plugins {
 }
 
 android {
-    namespace = "com.doorfast.captain"
+    namespace = "com.example.android"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-    
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -23,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.Door_Fast"
+        applicationId = "com.example.android"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -39,9 +37,6 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-}
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 flutter {
